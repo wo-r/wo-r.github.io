676e767d698c705deb8644dd6d2870dcbec883d5
wo-r <78428114+wo-r@users.noreply.github.com>
2024-12-19 10:28:09 -0600

    i did it, and added meta stuff for search engines

--- src/script.js
+++ src/script.js
@@ -358,22 +358,166 @@
     };
 
     const applyElements = async () => {
-        let slashes = window.location.pathname.split("/").length - 2;
-        let base = "./";
-        
-        for (let i = 0; i < slashes; i++) {
-            base += "../";
+        let slashPath = "./";
+        for (let i = 0; i < window.location.pathname.split("/").length - 2; i++) {
+            slashPath += "../";
         }
         
-        // TODO:
+        let elements = {
+            component: {
+                navbar: "",
+                sidemenu: "",
+                footer: "",
+            }
+        }
+
+        elements.component.navbar = `
+            <div class="flex flex-row gap-2">
+                <a id="printResume" type="navbar" tooltip="Quickly print out my entire resume for reviewing" class="cursor-pointer">
+                    <div ripple class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl z-20">
+                        <svg class="p-2 pt-[5px]" viewbox="-14 -1000 1000 1000" width="45px" height="45px">    
+                            <path class="fill-gray-300 pointer-events-none" d="M330-54q-57 0-96.5-38.8T194-190v-44h-30q-57 0-96.5-39.5T28-370v-144q0-68 47.04-116T189-678h582q68.17 0 114.59 48Q932-582 932-514v144q0 57-39.5 96.5T796-234h-30v44q0 58.4-39.5 97.2Q687-54 630-54H330Zm436-670H194v-29q0-58.4 39.5-97.2Q273-889 330-889h300q57 0 96.5 38.8T766-753v29Zm-76.5 321q25.5 0 42.5-16.81 17-16.82 17-41.69 0-25.5-16.81-43T689.5-522q-24.5 0-42 17.5t-17.5 43q0 24.5 17.5 41.5t42 17ZM330-183h300v-64H330v64Z">
+                        </svg>
+                    </div>
+                </a>
+                <a id="sidemenuToggle" type="open" class="cursor-pointer">
+                    <div class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl">
+                        <svg class="p-2 pt-[5px]" viewbox="0 0 50 45" width="45px" height="45px">    
+                            <path class="fill-gray-300 pointer-events-none" d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z">
+                        </svg>
+                    </div>
+                </a>
+            </div>
+            <div class="flex flex-row items-center">
+                <a goto="https://github.com/wo-r" class="cursor-pointer" tooltip="View my github page">
+                    <div ripple class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl">
+                        <img class="w-[45px] h-[45px] p-2 rounded-full" src="${slashPath}src/logo/logo-full.jpg">
+                    </div>
+                </a>
+            </div>
+        `;
+
+        elements.component.sidemenu = `
+            <div id="sidemenuBackdrop" class="hidden xl:hidden z-30 fixed inset-[-1000px] bg-black opacity-50 cursor-pointer"></div>
+            <div id="sidemenu" class="z-50 flex-shrink-0 absolute bottom-0 top-0 right-0 xl:relative overflow-x-hidden bg-brown-darker select-none" style="width: 0px;">
+                <div class="h-full w-[280px]">
+                    <div class="flex h-full min-h-0 flex-col">
+                        <div class="h-full w-full flex-1 items-start">
+                            <div class="flex h-full w-full flex-col">
+                                <div class="flex justify-between py-3 px-4">
+                                    <a goto="https://github.com/wo-r" class="cursor-pointer" tooltip="View my github page">
+                                        <div ripple class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl">
+                                            <img class="w-[45px] h-[45px] p-2 rounded-full" src="${slashPath}src/logo/logo-full.jpg">
+                                        </div>
+                                    </a>
+                                    <div class="flex flex-row gap-2">
+                                        <a id="printResume" type="sidemenu" tooltip="Quickly print out my entire resume for reviewing" class="cursor-pointer">
+                                            <div ripple class="relative overflow-hidden hover:bg-brown-light hover:bg-opacity-20 transition rounded-xl z-20">
+                                                <svg class="p-2 pt-[5px]" viewbox="-14 -1000 1000 1000" width="45px" height="45px">    
+                                                    <path class="fill-zinc-300 pointer-events-none" d="M330-54q-57 0-96.5-38.8T194-190v-44h-30q-57 0-96.5-39.5T28-370v-144q0-68 47.04-116T189-678h582q68.17 0 114.59 48Q932-582 932-514v144q0 57-39.5 96.5T796-234h-30v44q0 58.4-39.5 97.2Q687-54 630-54H330Zm436-670H194v-29q0-58.4 39.5-97.2Q273-889 330-889h300q57 0 96.5 38.8T766-753v29Zm-76.5 321q25.5 0 42.5-16.81 17-16.82 17-41.69 0-25.5-16.81-43T689.5-522q-24.5 0-42 17.5t-17.5 43q0 24.5 17.5 41.5t42 17ZM330-183h300v-64H330v64Z">
+                                                </svg>
+                                            </div>
+                                        </a>
+                                        <a id="sidemenuToggle" type="close" class="cursor-pointer">
+                                            <div class="relative overflow-hidden hover:bg-brown-light hover:bg-opacity-20 transition rounded-xl z-20">
+                                                <svg class="p-2 pt-[5px]" viewbox="0 0 50 45" width="45px" height="45px">    
+                                                    <path class="fill-zinc-300 pointer-events-none" d="M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z">
+                                                </svg>
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="flex flex-col gap-[10px] overflow-y-auto py-5">
+                                    <div class="flex flex-col gap-1 py-3 px-4">
+                                        <span class="font-black font-nunitoblack text-sm px-3 pb-2">OVERVIEW</span>
+                                        <a goto="/" class="cursor-pointer">
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Home
+                                            </div>
+                                        </a>
+                                        <a goto="/blogs/" class="cursor-pointer">
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Blogs
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="flex flex-col gap-1 py-3 px-4">
+                                        <span class="font-black font-nunitoblack text-sm px-3 pb-2">FOR BUSINESS</span>
+                                        <a goto="/resume/" class="cursor-pointer">
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Resume
+                                            </div>
+                                        </a>
+                                        <a goto="" class="cursor-pointer" disabled>
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Certifications
+                                            </div>
+                                        </a>
+                                        <a goto="" class="cursor-pointer" disabled>
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Experience
+                                            </div>
+                                        </a>
+                                        <a goto="" class="cursor-pointer" disabled>
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Education
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="flex flex-col gap-1 py-3 px-4">
+                                        <span class="font-black font-nunitoblack text-sm px-3 pb-2">CONTACT</span>
+                                        <a goto="" class="cursor-pointer" disabled>
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Give Feedback
+                                            </div>
+                                        </a>
+                                        <a goto="" class="cursor-pointer" disabled>
+                                            <div ripple class="relative overflow-hidden py-2 px-3 font-black hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 rounded-lg transition">
+                                                Email Me
+                                            </div>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        elements.component.footer = `
+            <div class="py-5 px-6 w-full">
+                <div class="flex justify-between items-center">
+                    <div class="flex flex-1">Copyright &copy; Wo-r 2019-<span id="currentYear"></span></div>
+                    <div class="flex flex-row gap-2 flex-2">
+                        <a goto="" tooltip="View my LinkedIn page to get a true full view of all my achievements" class="cursor-pointer" disabled>
+                            <div ripple class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl z-20">
+                                <svg class="p-2 pt-[3px]" viewbox="0 0 30 25" width="45px" height="45px">    
+                                    <path class="fill-gray-300 pointer-events-none" d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z">
+                                </svg>
+                            </div>
+                        </a>
+                        <a goto="https://www.youtube.com/@thatguywoods" tooltip="Check out my youtube channel where I make videos" class="cursor-pointer">
+                            <div ripple class="relative overflow-hidden hover:bg-brown-light hover:shadow-xl hover:bg-opacity-20 transition rounded-xl z-20">
+                                <svg class="p-2 pt-[3px]" viewbox="0 0 50 45" width="45px" height="45px">    
+                                    <path class="fill-gray-300 pointer-events-none" d="M 44.898438 14.5 C 44.5 12.300781 42.601563 10.699219 40.398438 10.199219 C 37.101563 9.5 31 9 24.398438 9 C 17.800781 9 11.601563 9.5 8.300781 10.199219 C 6.101563 10.699219 4.199219 12.199219 3.800781 14.5 C 3.398438 17 3 20.5 3 25 C 3 29.5 3.398438 33 3.898438 35.5 C 4.300781 37.699219 6.199219 39.300781 8.398438 39.800781 C 11.898438 40.5 17.898438 41 24.5 41 C 31.101563 41 37.101563 40.5 40.601563 39.800781 C 42.800781 39.300781 44.699219 37.800781 45.101563 35.5 C 45.5 33 46 29.398438 46.101563 25 C 45.898438 20.5 45.398438 17 44.898438 14.5 Z M 19 32 L 19 18 L 31.199219 25 Z">
+                                </svg>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        if ($("#navbar").length && $("#sidemenuContainer").length && $("#footer").length) {
+            await $("#navbar").append(elements.component.navbar);
+            await $("#sidemenuContainer").append(elements.component.sidemenu);
+            await $("#footer").append(elements.component.footer);
+        }
     }
 
     // Wait for document to be ready
     await $(window).ready(async function () {
-        // Set current year
-        $("#currentYear").text(new Date().getFullYear());
-
-        // Run all the initialization functions
         const tasks = [
             applyElements,
             fetchFollowersCount,
@@ -390,11 +534,15 @@
             setupTooltips,
             setupPopupText
         ];
-    
+        
+        // Run all the initialization functions
         for (let task of tasks) {
             try {
                 await task();
             } catch (e) {}
         }
+
+        // Set current year
+        $("#currentYear").text(new Date().getFullYear());
     })
 })();
